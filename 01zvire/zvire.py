d0fa61a223ddc3d1d36b2e1e7f355aafa4255f7c
lajdiss <ladislavcer@proton.me>
2026-09-14 12:21:16 +0200

    funkce

--- 01zvire/zvire.py
+++ 01zvire/zvire.py
@@ -4,8 +4,26 @@ class Zvire:
         self.vek = vek
         self.misto = misto
         pass
+    def zvuk(self):
+        return"??"
+    def predstav(self):
+        return f"ahoj jsem {self.jmeno}, je mi {self.vek}"
+    def kdeJsi(self):
+        return f"bydlim v {self.misto}"
+    
+    def bez(self, nMisto:str):
+        self.misto = nMisto
+        return f"presunul jsem se na {nMisto}. {self.kdeJsi()}"
+        
+    
 
-zvire = Zvire("Hund", 67,)
-print(zvire.jmeno)
-print(zvire.vek)
-print(zvire.misto)
+zvire = Zvire("Hund", 67)
+print(zvire.predstav())
+print(zvire.kdeJsi())
+print(zvire.zvuk())
+print(zvire.bez("obed"))
+
+
+zvire2 = Zvire("cokl", 0.5,"sklep")
+print(zvire2.predstav())
+print(zvire.kdeJsi())
